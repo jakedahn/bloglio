fc7f8d93c1f16ee558541f36dbcc73ad25841921
Jake Dahn <admin@jakedahn.com>
2009-10-29 03:02:31 -0500

    changing db settings, trying to get things running on heroku

---
+++ config/config.rb
@@ -0,0 +1,19 @@
+def connect_to_db(mode)
+  dbconfig = YAML.load(File.read('config/database.yml'))
+  ActiveRecord::Base.establish_connection dbconfig['development']
+end
+
+def common_config(mode)
+  connect_to_db(mode)
+
+  enable :sessions
+end
+
+configure :development do  
+  common_config(:development)
+  set :port, 31337
+end
+
+configure :test do
+  common_config(:test)
+end
