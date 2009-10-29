ac17c6b94dce45019d0ebe26abc7159f6ade9277
Jake Dahn <admin@jakedahn.com>
2009-10-29 06:17:16 -0500

    maybe this

--- config/config.rb
+++ config/config.rb
@@ -1,21 +1,2 @@
-def connect_to_db(mode)
-  dbconfig = YAML.load(File.read('config/database.yml'))
-  ActiveRecord::Base.establish_connection dbconfig["production"]
-  
-end
-
-def common_config(mode)
-  connect_to_db(mode)
-
-  enable :sessions
-end
-
-configure :development do  
-  common_config(:development)
-  set :port, 31337
-end
-
-
-configure :test do
-  common_config(:test)
-end
+dbconfig = YAML.load(File.read('config/database.yml'))
+ActiveRecord::Base.establish_connection dbconfig["production"]
