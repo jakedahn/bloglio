499726c58d82a31a00bb023936a9df1d48504b5e
Jake Dahn <admin@jakedahn.com>
2009-10-29 02:34:49 -0500

    test

--- blog.rb
+++ blog.rb
@@ -1,11 +1,10 @@
 require 'rubygems'
 require 'sinatra'
-require 'twilio'
-
 require 'redcloth'
 require 'sass'
 require 'hpricot'
 require 'activerecord'
+require 'twilio'
 
 load 'config.rb'
 load 'models.rb'
