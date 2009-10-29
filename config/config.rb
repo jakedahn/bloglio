4ffbbc00dd7f94a4b87130ed618b26feaecc932d
Jake Dahn <admin@jakedahn.com>
2009-10-29 03:25:37 -0500

    testing change in config

--- config/config.rb
+++ config/config.rb
@@ -1,6 +1,9 @@
 def connect_to_db(mode)
-  dbconfig = YAML.load(File.read('config/database.yml'))
-  ActiveRecord::Base.establish_connection dbconfig['development']
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :encoding => 'unicode',
+    :database => 'blog_development'
+  )
 end
 
 def common_config(mode)
